493ab88221399084677c1855e4b8288088a9441a
Christoph Schuhmann <christoph_s@freenet.de>
2022-06-26 09:32:50 +0200

    Update prep-logos-oai.py

--- prep-logos-oai.py
+++ prep-logos-oai.py
@@ -1,5 +1,7 @@
 
-
+# This script prepares the training images and ratings for the training.
+# It assumes that all images are stored as files that PIL can read.
+# It also assumes that the paths to the images files and the average ratings are in a .parquet files that can be read into a dataframe ( df ).
 
 from datasets import load_dataset
 import pandas as pd
@@ -20,15 +22,12 @@ def normalized(a, axis=-1, order=2):
 
 
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-L/14", device=device)
 
-
-f = "/mnt/spirit/logos/00000.parquet"
-df = pd.read_parquet(f)
-
-
+ 
+f = "trainingdata.parquet"
+df = pd.read_parquet(f)  #assumes that the df has the columns IMAGEPATH  & AVERAGE_RATING
 
 
 x = []
@@ -38,12 +37,12 @@ c= 0
 for idx, row in df.iterrows():
     start = time.time()
 
-    mean_val = float(row.preference_average)
-    print(mean_val)
-    if mean_val <1:
+    average_rating = float(row.AVERAGE_RATING)
+    print(average_rating)
+    if average_rating <1:
        continue
 
-    img= "/opt/AVA_dataset/AVA_dataset/images/images/" + str(idx)+".jpg"
+    img= row.IMAGEPATH  #assumes that the df has the column IMAGEPATH
     print(img)
 
     try:
@@ -55,17 +54,14 @@ for idx, row in df.iterrows():
        image_features = model.encode_image(image)
 
     im_emb_arr = image_features.cpu().detach().numpy() 
-    x.append(normalized ( im_emb_arr) )
+    x.append(normalized ( im_emb_arr) )      # all CLIP embeddings are getting normalized. This also has to be done when inputting an embedding later for inference
     y_ = np.zeros((1, 1))
-    y_[0][0] = mean_val
-    #y_[0][1] = stdev 
+    y_[0][0] = average_rating
+    #y_[0][1] = stdev      # I initially considered also predicting the standard deviation, but then didn't do it
 
     y.append(y_)
-    #print(im_emb_arr.shape )
-    #print( y_ )
-    
-    #print(idx)
-    #print(time.time()-start) 
+
+
     print(c)
     c+=1
 
@@ -76,5 +72,5 @@ x = np.vstack(x)
 y = np.vstack(y)
 print(x.shape)
 print(y.shape)
-np.save('ava_x_logos_oai.npy', x)
-np.save('ava_y_logos_oai.npy', y)
+np.save('x_OpenAI_CLIP_L14_embeddings.npy', x)
+np.save('y_ratings.npy', y)
