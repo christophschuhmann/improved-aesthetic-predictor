e283bf5b2ec258d4a055b072f48b4a82f75bda76
Christoph Schuhmann <christoph_s@freenet.de>
2022-06-26 09:51:26 +0200

    Update avalogoclip.py

--- avalogoclip.py
+++ avalogoclip.py
@@ -1,7 +1,7 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+# os.environ['CUDA_VISIBLE_DEVICES'] = "0"       # in case you are using a multi GPU workstation, choose your GPU here
 import tqdm
-import pytorch_lightning as pl
+
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -11,6 +11,8 @@ from datasets import load_dataset
 from torch.utils.data import TensorDataset, DataLoader
 
 import numpy as np
+
+#define your neural net here:
 
 class MLP(pl.LightningModule):
     def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
@@ -58,40 +60,32 @@ class MLP(pl.LightningModule):
 
 
 
+# load the training data 
 
-x = np.load ("/mnt/spirit/ava_x.npy") #_openclip_l14.npy")
+x = np.load ("/mnt/spirit/ava_x.npy")
 
-y = np.load ("/mnt/spirit/ava_y.npy") #_openclip_l14.npy")
+y = np.load ("/mnt/spirit/ava_y.npy")
 
-x2 = np.load ("/mnt/spirit/x_logos_oai.npy") #_openclip_l14.npy")
+val_percentage = 0.05 # 5% of the trainingdata will be used for validation
 
-y2 = np.load ("/mnt/spirit/y_logos_oai.npy") #_openclip_l14.npy")
+train_border = int(x.shape()[0] * (1 - val_percentage) )
 
-x =np.concatenate((x,x2),axis = 0)
-y =np.concatenate((y,y2),axis = 0)
-
-from sklearn.utils import shuffle
-x, y = shuffle(x, y)
-
-
-
-train_tensor_x = torch.Tensor(x[:262000]) # transform to torch tensor
-train_tensor_y = torch.Tensor(y[:262000])
+train_tensor_x = torch.Tensor(x[:train_border]) # transform to torch tensor
+train_tensor_y = torch.Tensor(y[:train_border])
 
 train_dataset = TensorDataset(train_tensor_x,train_tensor_y) # create your datset
 train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True,  num_workers=16) # create your dataloader
 
 
-val_tensor_x = torch.Tensor(x[262000:]) # transform to torch tensor
-val_tensor_y = torch.Tensor(y[262000:])
+val_tensor_x = torch.Tensor(x[train_border:]) # transform to torch tensor
+val_tensor_y = torch.Tensor(y[train_border:])
 
+'''
 print(train_tensor_x.size())
-
 print(val_tensor_x.size())
-
 print( val_tensor_x.dtype)
 print( val_tensor_x[0].dtype)
-
+'''
 
 val_dataset = TensorDataset(val_tensor_x,val_tensor_y) # create your datset
 val_loader = DataLoader(val_dataset, batch_size=512,  num_workers=16) # create your dataloader
@@ -99,20 +93,22 @@ val_loader = DataLoader(val_dataset, batch_size=512,  num_workers=16) # create y
 
 
 
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+model = MLP(768).to(device)   # CLIP embedding dim is 768 for CLIP ViT L 14
 
-model = MLP(768).to(device)
+optimizer = torch.optim.Adam(model.parameters()) 
 
-optimizer = torch.optim.Adam(model.parameters()) #, lr=1e-3
+# choose the loss you want to optimze for
 criterion = nn.MSELoss()
 criterion2 = nn.L1Loss()
 
-epochs = 30
+epochs = 50
 
 model.train()
 best_loss =999
+save_name = "predictor_L14_MSE.pth"
+
 
 for epoch in range(epochs):
     losses = []
@@ -162,20 +158,20 @@ for epoch in range(epochs):
     print('Validation - Epoch %d | MSE Loss %6.2f' % (epoch, sum(losses)/len(losses)))
     print('Validation - Epoch %d | MAE Loss %6.2f' % (epoch, sum(losses2)/len(losses2)))
     if sum(losses)/len(losses) < best_loss:
-        print("Best MAE Val loss so far. Saving model ava+logos-l14-reluMSE.pth")
+        print("Best MAE Val loss so far. Saving model")
         best_loss = sum(losses)/len(losses)
         print( best_loss ) 
-        torch.save(model, "ava+logos-l14-reluMSE.pt")
-        torch.save(model.state_dict(), "ava+logos-l14-reluMSE.pth")
+
+        torch.save(model.state_dict(), save_name )
 
 
-
-
-#torch.save(model, "ava+logos-l14-reluMSE.pt")
-#torch.save(model.state_dict(), "ava+logos-l14-reluMSE.pth")
+torch.save(model.state_dict(), save_name)
 
 print( best_loss ) 
 
+print("training done")
+# inferece test with dummy samples from the val set, sanity check
+print( "inferece test with dummy samples from the val set, sanity check")
 model.eval()
 output = model(x[:5].to(device))
 print(output.size())
